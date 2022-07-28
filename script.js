02e9c09057e2a399559cfe4c9715bde08eaf5daf
“Jefferson <jefferson-thales@hotmail.com>
2022-07-28 15:52:10 -0300

    Alteração avaliações e carrocel de imagens

--- script.js
+++ script.js
@@ -18,6 +18,30 @@ document.querySelectorAll(".image-slider img").forEach(images =>{
     images.onclick = () =>{
         var src = images.getAttribute("src");
         document.querySelector(".main-home-image").src = src;
-    }
+    };
 });
 //Trocar images - FIM
+
+//Carrocel de imagens - inicio
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop : true,
+    grabCursor: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+    },
+});
+//Carrocel de imagens - Fim
